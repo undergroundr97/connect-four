693227fb448e7f8884bbe8fc28a8688d1dc2380e
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-02-24 13:40:36 -0300

    feat: added display board to game within the game module

--- lib/game.rb
+++ lib/game.rb
@@ -17,6 +17,19 @@ class ConnectFour
   def change_player
     @current == @player1 ? @current = @player2 : @current = @player1
   end
+  def draw?
+     @board.board.none?{|place| place == ' '}
+  end
+  def display_board
+    puts "0 1 2 3 4 5"
+    (0..6).each do |row|
+      (0..5).each do |col|
+        print @board.board[col][row] == ' ' ? "\u25CB " : "#{@board.board[col][row]} "
+      end
+      puts row
+    end
+  end
+
 
 end
 
@@ -32,4 +45,8 @@ c.play(5)
 c.play(5)
 c.play(5)
 
-p c.board.board
+# p c.board.board
+puts c.display_board
+
+c.play(4)
+c.display_board
