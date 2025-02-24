12c0389e82f6cad47baa11aeea6ca8f9e79817c9
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-02-24 14:18:22 -0300

    feat: winner function, vertically, while loop to asecure input inside 0..6 range

--- lib/game.rb
+++ lib/game.rb
@@ -9,6 +9,8 @@ class ConnectFour
     @board = Board.new
   end
   def play(col)
+    return false if col > 5 || col < 0
+    return false if !@board.board[col].include?(" ")
     current_player = @current
     row = @board.board[col].rindex(' ')
     @board.board[col][row] = current_player
@@ -18,7 +20,7 @@ class ConnectFour
     @current == @player1 ? @current = @player2 : @current = @player1
   end
   def draw?
-     @board.board.none?{|place| place == ' '}
+     return true if @board.board.flatten.all?{|place| place != ' '} && winner?
   end
   def display_board
     puts "0 1 2 3 4 5"
@@ -29,13 +31,36 @@ class ConnectFour
       puts row
     end
   end
-
+  def winner?
+    @board.board.each do |col|
+      col.each_cons(4) do |four|
+         four
+        return true if four.uniq.size == 1 && !four.include?(' ')
+      end
+    end
+  end
   def play_game
     loop do
-    display_board    
+    display_board 
+    to_check = (0..5).to_a   
     puts "Where you want to play your token #{@current}?"
     col = gets.chomp.to_i
+    while !to_check.include?(col)
+      puts "Invalid move! do it again!"
+      col = gets.chomp.to_i
+    end
     play(col)
+    if winner? == true
+      change_player
+      puts "Congratulations #{@current}, you won the game!"
+      display_board
+      break
+    end
+    if draw? == true
+      display_board
+      puts "It's a draw!"
+      break
+    end
     end
   end
 
